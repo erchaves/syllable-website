0a9590022277fd98b78508e59cc621bb6cbd8d62
Sara Corenzwit <>
2019-01-14 14:56:28 -0500

    hover animation for side nav (WIP)

--- src/Components/NavSide/index.js
+++ src/Components/NavSide/index.js
@@ -9,20 +9,23 @@ class NavSide extends React.Component {
           <div className="bar">
             <div className="menu">
               <a className="menu-item" href="https://www.behance.net/erchaves">
-                <span className="icon-behance"></span>
+                <span className="menu-item__icon icon-behance"></span>
+                <span className="menu-item__label">Portfolio</span>
               </a>
               <a className="menu-item" href="https://medium.com/@syllable">
-                <span className="icon-medium"></span>
+                <span className="menu-item__icon icon-medium"></span>
+                <span className="menu-item__label">Blog</span>
               </a>
               <a className="menu-item" href="https://twitter.com/syllable_team">
-                <span className="icon-twitter"></span>
+                <span className="menu-item__icon icon-twitter"></span>
+                <span className="menu-item__label">Twitter</span>
               </a>
             </div>
           </div>
         </div>
+
         <div className="bar-wrap right">
           <div className="bar">
-            right
           </div>
         </div>
       </div>
