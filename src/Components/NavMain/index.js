566cdb594f7dec2d9cbd7aff8891c0ac9b49b666
Sara Corenzwit <>
2019-01-21 13:19:31 -0500

    updated main nav

--- src/Components/NavMain/index.js
+++ src/Components/NavMain/index.js
@@ -4,21 +4,22 @@ import './index.scss';
 class NavMain extends React.Component {
   render() {
     return (
-      <div className="nav-main">
-        <ul className="flex-wrap">
+      <nav className="nav-main">
+        <a className="nav-logo" href="/">
+          <img className={`syllable-logo`} src={`/syllable-logo.png`} />
+        </a>
+        <ul className="flex-wrap nav-items">
+          <li className="item-secondary flex-item">
+            <a href="/about">Profile</a>
+          </li>
           <li className="item-secondary flex-item">
             <a href="/projects">Projects</a>
           </li>
-          <li className="item-primary flex-item">
-            <a href="/">
-              <img className={`syllable-logo`} src={`/syllable-logo.png`} />
-            </a>
-          </li>
           <li className="item-secondary flex-item">
-            <a href="/about">About</a>
+            <a href="https://medium.com/@syllable">Blog</a>
           </li>
         </ul>
-      </div>
+      </nav>
     );
   }
 }
